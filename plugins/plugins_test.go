ecaa6e78e9f9991220b8ea47eee05784c360ae94
mritd <mritd@linux.com>
2019-05-23 10:54:59 +0800

    test(plugins): add test
    add test
    
    Signed-off-by: mritd <mritd@linux.com>

--- plugins/plugins_test.go
+++ plugins/plugins_test.go
@@ -2,8 +2,17 @@ package plugins
 
 import "testing"
 
+func TestFindPlugins(t *testing.T) {
+	ps := FindPlugins("geoip", "authz", "ipfilter")
+	if len(ps) != 3 {
+		t.Fatal("plugins missing")
+	} else {
+		t.Log(ps)
+	}
+}
+
 func TestGeneratePluginsCode(t *testing.T) {
-	err := GeneratePluginsCode([]string{"geoip", "authz", "ipfilter"})
+	err := GeneratePluginsCode("geoip", "authz", "ipfilter")
 	if err != nil {
 		t.Fatal(err)
 	}
