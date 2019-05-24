95105ebe6883105c5a901cc43a50b9f06e945447
mritd <mritd@linux.com>
2019-05-24 11:30:34 +0800

    feat(version): add banner
    add banner
    
    Signed-off-by: mritd <mritd@linux.com>

--- version.go
+++ version.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"runtime"
 
 	"github.com/spf13/cobra"
 )
 
-var versionTpl = `
-Name: caddybuilder
+var bannerBase64 = "CiAgX19fICAgICAgICAgICAgICAgICAgICAgICAgICAgICwgX18gICAgICAgICAgICBfICAgICAgICAgICAgICAgICAgCiAvIChfKSAgICAgICAgIHwgICAgIHwgICAgICAgICAgL3wvICBcICAgICAgIG8gIHwgfCAgICB8ICAgICAgICAgICAgCnwgICAgICBfXywgICBfX3wgICBfX3wgICAgICAgICAgIHwgX18vICAgICAgICAgIHwgfCAgX198ICAgXyAgICxfICAgCnwgICAgIC8gIHwgIC8gIHwgIC8gIHwgIHwgICB8ICAgIHwgICBcfCAgIHwgIHwgIHwvICAvICB8ICB8LyAgLyAgfCAgCiBcX19fL1xfL3xfL1xfL3xfL1xfL3xfLyBcXy98LyAgIHwoX18vIFxfL3xfL3xfL3xfXy9cXy98Xy98X18vICAgfF8vCiAgICAgICAgICAgICAgICAgICAgICAgICAgIC98ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgIFx8ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgCgo="
+
+var versionTpl = `%s
 Version: %s
 Arch: %s
 BuildDate: %s
@@ -27,7 +29,8 @@ var versionCmd = &cobra.Command{
 	Long: `
 Print version.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf(versionTpl, Version, runtime.GOOS+"/"+runtime.GOARCH, BuildDate, CommitID)
+		banner, _ := base64.StdEncoding.DecodeString(bannerBase64)
+		fmt.Printf(versionTpl, banner, Version, runtime.GOOS+"/"+runtime.GOARCH, BuildDate, CommitID)
 	},
 }
 
